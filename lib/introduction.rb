cc49053627d15925cde7dadb47ffb89b3ede9c17
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-01 18:22:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,9 +8,12 @@ def introduction_with_language(name, language)
   puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Taylor", "Ruby")
+introduction_with_language("Taylor", "Javascript")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Joe", "Ruby")
   
   
